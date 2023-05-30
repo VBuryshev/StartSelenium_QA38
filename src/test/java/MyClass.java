b86d29fa451d51db550b3cfc2aa8d272733cdb6f
VBuryshev <v.buryshev@gmail.com>
2023-05-30 21:05:27 +0300

    CW02
    End of the lesson 02

---
+++ src/test/java/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to automatization course");
+        System.out.println("I know u already forget a JAVA syntax,but don't worry, if u was a good student of course BP all will be alright");
+    }
+}
---
+++ src/test/java/Start_Selenium.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import sun.security.mscapi.CPublicKey;
+
+public class Start_Selenium
+{
+    WebDriver wd;
+
+    @BeforeTest
+    public void precondition()
+    {
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/home");
+    }
+
+    @Test
+    public void test()
+    {
+
+    }
+    @AfterTest
+    public void postcondition()
+    {
+
+    }
+}
